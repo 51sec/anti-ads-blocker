a87a608677d1993a1dc8303299e9a46c1164efc1
51sec <johnyan.ca@gmail.com>
2023-03-11 11:23:10 -0500

    Create 2.middle.js

---
+++ 2.middle.js
@@ -0,0 +1,39 @@
+<style>
+#iaMFGndsgOhR {
+display: none;
+top: 50%;
+left: 50%;
+width: 50em;
+height: 25em;
+margin-top: -20em;
+margin-left: -20em;
+border: 1px solid #666;
+background: #D30000;
+position: fixed;
+margin-bottom: 30px;
+padding: 20px 10px;
+font-size: large;
+text-align: center;
+font-weight: bold;
+color: #fff;
+border-radius: 5px;
+}
+</style>
+
+<div id="iaMFGndsgOhR">
+<br>
+  Our website is made possible by displaying online advertisements to our visitors.<br><br><br>
+  Please consider supporting us by disabling your ad blocker.<br><br><br>
+  Please turn off your Adsblocker to support 51sec!<br><br><br>
+  Thank you!<br><br>
+  51sec.org!
+</div>
+
+<script src="https://51sec.org/adex.js" type="text/javascript"></script>
+<script type="text/javascript">
+
+if(!document.getElementById('eNmOUiSgyxfM')){
+  document.getElementById('iaMFGndsgOhR').style.display='block';
+}
+
+</script>
