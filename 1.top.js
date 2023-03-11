28e1d666e46d30e4ff6d6900f5171d3fca063784
51sec <johnyan.ca@gmail.com>
2023-03-11 11:22:39 -0500

    Create 1.top.js

---
+++ 1.top.js
@@ -0,0 +1,28 @@
+<style>
+#iaMFGndsgOhR {
+display: none;
+margin-bottom: 30px;
+padding: 20px 10px;
+background: #D30000;
+text-align: center;
+font-weight: bold;
+color: #fff;
+border-radius: 5px;
+}
+</style>
+
+
+
+<div id="iaMFGndsgOhR">
+
+  Our website is made possible by displaying online advertisements to our visitors.<br>
+  Please consider supporting us by disabling your ad blocker. 51sec.org!
+</div>
+
+<script src="https://51sec.org/adex.js" type="text/javascript"></script>
+<script type="text/javascript">
+
+if(!document.getElementById('eNmOUiSgyxfM')){
+  document.getElementById('iaMFGndsgOhR').style.display='block';
+}
+</script>
